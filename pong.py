983881d4f57f1212c2813d5750a441e409311363
Vivek <viiivek1@gmail.com>
2019-12-10 10:36:49 +0530

    some more features added

--- pong.py
+++ pong.py
@@ -41,10 +41,69 @@ ball.shape("circle")
 ball.color("white")
 ball.penup()
 ball.goto(0,0)
+ball.dx=3
+ball.dy=3
+
+#Functions
+def paddle_a_up():
+    y=paddle_a.ycor()
+    y+=20
+    paddle_a.sety(y)
+
+def paddle_a_down():
+    y=paddle_a.ycor()
+    y-=20
+    paddle_a.sety(y)
+
+def paddle_b_up():
+    y=paddle_b.ycor()
+    y+=20
+    paddle_b.sety(y)
+
+def paddle_b_down():
+    y=paddle_b.ycor()
+    y-=20
+    paddle_b.sety(y)
+
+# Keyboard binding
+wn.listen()
+wn.onkeypress(paddle_a_up,"w") #Lowercase "w" will only work not the upper case
+wn.onkeypress(paddle_a_down,"s")
+wn.onkeypress(paddle_b_up,"Up")
+wn.onkeypress(paddle_b_down,"Down")
+
 
 
 #Main game loop
 
 while True:
-    wn.update() #tap = 4 spaces keys
+    wn.update() #tap = 4 spaces keys use either not both otherwise you may face some error
+
+
+    #Move the ball
+    ball.setx(ball.xcor() + ball.dx)
+    ball.sety(ball.ycor() + ball.dy)
+
+    #Border checking
+    if ball.ycor() > 290 :
+        ball.sety(290)
+        ball.dy *=-1
+
+    if ball.ycor() < -290:
+        ball.sety(-290)
+        ball.dy *=-1
+
+    if ball.xcor() > 390:
+        ball.setx(0)
+        ball.dx *=-1
+
+    if ball.xcor() < -390:
+        ball.setx(0)
+        ball.dx *=-1
+
+    
+
+
+    
+
 
